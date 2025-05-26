c37389b6ce0e93b95cb0b432359b5ff60ad4cd14
Rael <santosrafaelpro@gmail.com>
2025-05-26 16:06:58 -0300

    pdf merger concluido

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# PDF_merger
+## 📚 Simple PDF Merger
 
+Este script Python super simples te ajuda a juntar vários arquivos PDF em um só! Organize seus documentos de forma fácil e rápida. ✨
+
+---
+
+### 🛠️ Como Usar
+
+1.  **Instale os `requerimentos`**:
+    Primeiro, certifique-se de que você tem todas as bibliotecas necessárias. Você pode instalá-las usando o arquivo `requirements.txt`:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+2.  **Organize seus PDFs**:
+    Crie uma pasta chamada `PDFs` (no mesmo lugar onde está o script `app.py`) e coloque *todos os arquivos PDF que você quer mesclar* dentro dela.
+
+3.  **Execute o script**:
+    Vá até a pasta do seu projeto no terminal e execute o script:
+    ```bash
+    python app.py
+    ```
+
+---
+
+### 📁 Onde os PDFs Vão Parar?
+
+* **PDFs de Entrada:** Você deve colocar todos os seus PDFs a serem mesclados na pasta `PDFs`.
+* **PDF Mesclado:** O arquivo PDF final, chamado `merged.pdf`, será salvo em uma nova pasta chamada `PDFs merged` (que será criada automaticamente se não existir), dentro do diretório do seu projeto.
+
+---
+
+Aproveite a organização dos seus PDFs! 🎉
---
+++ app.py
@@ -0,0 +1,48 @@
+import PyPDF2
+import sys
+import os
+
+''' Configuração das pastas'''
+
+INPUT_FOLDER = 'PDFs'
+OUTPUT_FOLDER = 'PDFs_merged'
+OUTPUT_FILENAME = 'merged.pdf'
+
+''' Criação das pastas se não existirem '''
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
+    print(f'Pasta {OUTPUT_FOLDER} criada com sucesso!')
+
+merger = PyPDF2.PdfMerger()
+pdfs_encontrados = False
+
+print(f'Procurando arquivos PDF na pasta {INPUT_FOLDER}...')
+
+"""Core logic"""
+
+for filename in os.listdir(INPUT_FOLDER):
+    if filename.endswith('.pdf'):
+        filepath = os.path.join(INPUT_FOLDER, filename)
+        
+        print(f'Adicionando {filename}...')
+        try:
+            merger.append(filepath)
+            pdfs_encontrados = True
+        except PyPDF2.utils.PdfReadError:
+            print(f'Erro ao ler o arquivo {filename}. Verifique se ele está corrompido ou protegido por senha.')
+        except Exception as e:
+            print(f'Ocorreu um erro ao processar o arquivo {filename}: {e}')
+            
+""" Salvando o PDF mesclado em OUTPUT_FOLDER """
+
+if pdfs_encontrados:
+    output_filepath = os.path.join(OUTPUT_FOLDER, OUTPUT_FILENAME)
+    try:
+        merger.write(output_filepath)
+        print(f'PDFs mesclados com sucesso em {output_filepath}!')
+    except Exception as e:
+        print(f'Ocorreu um erro ao salvar o {OUTPUT_FILENAME} : {e}')
+else:
+    print(f'Nehum arquivo PDF encontrado na pasta {INPUT_FOLDER}. Verifique se os arquivos estão no formato correto.')
+    
+merger.close()
